d7c3bfa019b4e9e53bb5265019b0512a58036d98
Mateusz Wojtczak <wojtczakmat@gmail.com>
2018-04-24 02:17:23 +0200

    Eject expo project and add basic connectivity

--- GameLobby/GameSelection.js
+++ GameLobby/GameSelection.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import NearbyConnection, {CommonStatusCodes, ConnectionsStatusCodes, Strategy, Payload, PayloadTransferUpdate} from 'react-native-google-nearby-connection';
 
 export default class GameSelection extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { selectedGame: undefined }
+        this.state = { 
+            selectedGame: undefined, 
+            availableGames: [] }
     }
 
-    fakeGames = [{quiz: "Favourite meals", owner: "Kinga"}, {quiz: "Scary", owner: "Adam"}];
+    componentDidMount() {
+        NearbyConnection.onEndpointDiscovered(({
+            endpointId,             // ID of the endpoint wishing to connect
+            endpointName,           // The name of the remote device we're connecting to.
+            serviceId               // A unique identifier for the service
+        }) => {
+            // An endpoint has been discovered we can connect to
+            console.warn("endpoint discovered: " + endpointId + " " + endpointName);
+            this.state.availableGames.push({ id: endpointId, owner: endpointName });
+            
+            this.setState({
+                availableGames: this.state.availableGames 
+            });
+        });
+
+        NearbyConnection.startDiscovering(
+            "KQUIZ",
+            Strategy.P2P_STAR                // A unique identifier for the service
+        );
+    }
+
+    //fakeGames = [{quiz: "Favourite meals", owner: "Kinga"}, {quiz: "Scary", owner: "Adam"}];
 
     render() {
         const { params } = this.props.navigation.state;
@@ -18,10 +42,10 @@ export default class GameSelection extends React.Component {
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Join Game : </Text>
                 {
-                    this.fakeGames.map(game => <View key={game.quiz + game.owner} style={{ padding: 5 }}>
+                    this.state.availableGames.map(game => <View key={game.id + game.owner} style={{ padding: 5 }}>
                         <Button
                             color={this.state.selectedGame === game ? "#673AB7" : "#2196F3"}
-                            title={game.quiz + " by " + game.owner}
+                            title={game.id + " by " + game.owner}
                             onPress={() => this.selectGame(game)} />
                     </View>)
                 }
@@ -31,6 +55,8 @@ export default class GameSelection extends React.Component {
                         title="Start Game"
                         color="#F44336"
                         onPress={() => {
+                            NearbyConnection.connectToEndpoint("KQUIZ", this.state.selectedGame.id);
+
                             this.props.navigation.navigate('Waiting', {
                               username: username,
                               quiz: this.state.selectedGame.quiz,
@@ -42,6 +68,7 @@ export default class GameSelection extends React.Component {
         );
     }
 
-    selectGame = (game) => this.setState({ selectedGame: game });
-    
+    selectGame = (game) => {
+        this.setState({ selectedGame: game });
+    }
 }
--- GameLobby/PlayerSelection.js
+++ GameLobby/PlayerSelection.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import NearbyConnection, {CommonStatusCodes, ConnectionsStatusCodes, Strategy, Payload, PayloadTransferUpdate} from 'react-native-google-nearby-connection';
 
 export default class PlayerSelection extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { selectedPlayer: undefined }
+        this.state = { 
+            params: props.navigation.state,    
+            selectedPlayer: undefined,
+            players: []
+        }
     }
 
-    fakePlayers = ["Wojtek", "Jasiek", "Kinga"];
+    componentDidMount() {
+        NearbyConnection.onConnectionInitiatedToEndpoint(({
+            endpointId,             // ID of the endpoint wishing to connect
+            endpointName,           // The name of the remote device we're connecting to.
+            authenticationToken,    // A small symmetrical token that has been given to both devices.
+            serviceId,              // A unique identifier for the service
+            incomingConnection      // True if the connection request was initated from a remote device.
+        }) => {
+            // Connection has been initated
+
+            this.state.players.push({id: endpointId, name: endpointName});
+
+            this.setState({
+                players: this.state.players
+            });
+        });
+        
+        NearbyConnection.startAdvertising(
+            this.state.params.username,               // This nodes endpoint name
+            "KQUIZ",              // A unique identifier for the service
+            Strategy.P2P_STAR     // The Strategy to be used when discovering or advertising to Nearby devices [See Strategy](https://developers.google.com/android/reference/com/google/android/gms/nearby/connection/Strategy)
+        );
+    }
+
+    //fakePlayers = ["Wojtek", "Jasiek", "Kinga"];
 
     render() {
         const { params } = this.props.navigation.state;
@@ -19,10 +48,10 @@ export default class PlayerSelection extends React.Component {
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Select Player : </Text>
                 {
-                    this.fakePlayers.map(player => <View key={player} style={{ padding: 5 }}>
+                    this.state.players.map(player => <View key={player.id} style={{ padding: 5 }}>
                         <Button
                             color={this.state.selectedPlayer === player ? "#673AB7" : "#2196F3"}
-                            title={player}
+                            title={player.name}
                             onPress={() => this.selectPlayer(player)} />
                     </View>)
                 }
@@ -34,7 +63,7 @@ export default class PlayerSelection extends React.Component {
                         onPress={() => {
                             this.props.navigation.navigate('QuizStart', {
                               username: username,
-                              friendname: this.state.selectedPlayer,
+                              friendname: this.state.selectedPlayer.name,
                               quiz: quiz
                             });
                           }} />

cc5d71464a87af0d7584eb012ae5e4e22347309a
tokarskaa <tokarskaala@gmail.com>
2018-06-05 20:52:18 +0200

    fix waiting

--- GameLobby/Waiting.js
+++ GameLobby/Waiting.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { gameService } from '../communication/GameService.js';
-import getQuiz from "../QuizStorage";
+import { quizDatabase} from '../QuizTest';
 
 export default class Waiting extends React.Component {
 
@@ -9,7 +9,7 @@ export default class Waiting extends React.Component {
         super(props);
         const { params } = props.navigation.state;
 
-        this.state = { playerAccepted: false, gameId: params.gameId, quiz: undefined };
+        this.state = { playerAccepted: false, gameId: params.gameId};
     }
 
     componentDidMount() {
@@ -23,57 +23,16 @@ export default class Waiting extends React.Component {
                     const username = params ? params.username : null;
                     const friendname = params ? params.friendname : null;
                     this.setState({ playerAccepted: true });
-                    const q = await this.downloadQuiz(quizId);
-                    console.log(q);
                     this.props.navigation.navigate('QuizStart', {
                         username: username,
-                        quiz: q,
+                        quiz: quizDatabase.quizzes.find(quiz => quiz.id === quizId),
                         friendname: friendname
                     });
                 })
             }
         );
     }
-
-    downloadQuiz = async (key) => {
-        return await getQuiz(key);
-    }
-
-    fakeQuizes = [
-        {
-            id: 1,
-            title: "Favourite meals",
-            questions: [
-                {
-                    text: "What is the best italian dish ?",
-                    answers: ["Spaghetti", "Carbonara", "Pizza", "Lazagne"]
-                },
-                {
-                    text: "What is the best polish food ?",
-                    answers: ["Bigos", "Pierogi", "Schabowe", "Gołąbki"]
-                },
-                {
-                    text: "Favourite cusine ?",
-                    answers: ["Polish", "Italian", "Thai", "French"]
-                }
-            ]
-        },
-        {
-            id: 2,
-            title: "Scary",
-            questions: [
-                {
-                    text: "The most scary are ?",
-                    answers: ["clowns", "snakes", "spiders", "debts"]
-                },
-                {
-                    text: "What is you favourite monster ?",
-                    answers: ["Alien", "Dracula", "Predator", "Werewolf"]
-                }
-            ]
-        }
-    ];
-
+   
     render() {
         const { params } = this.props.navigation.state;
         const username = params ? params.username : null;
